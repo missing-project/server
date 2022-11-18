ac8eb6716af0a491dc3abd2ec844c3af4b60c00e
SonByungjin <sgyos000@gmail.com>
2022-11-18 18:36:07 +0900

    Feature: 로거 추가

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,8 @@
 import { ErrorRequestHandler } from 'express';
 import { errorResponse } from '../utils';
+import { logger } from '../winston';
 
 export const errorHandler: ErrorRequestHandler = (err, req, res) => {
-  console.log('\x1b[33m%s\x1b[0m', err.stack);
+  logger.error('\x1b[33m%s\x1b[0m', err.stack);
   errorResponse(res, 'BADREQUEST', err.message);
 };
