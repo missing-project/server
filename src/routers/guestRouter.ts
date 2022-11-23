be7c5780452c348f93d5ca748337f9c523812b41
sangwooyu <dbtkddn598@gmail.com>
2022-11-23 09:42:13 +0900

    Feature: guest 라우터 생성 및 로그인 회원가입 구현 token 발행구현

---
+++ src/routers/guestRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { guestController } from '../controller';
+import { asyncHandler } from '../utils';
+
+export const guestRouter = Router();
+guestRouter.get('/home',(req,res)=>{
+    res.send('hello Guest!')
+})
+guestRouter.get('/', asyncHandler(guestController.loginUser));
+guestRouter.post('/',asyncHandler(guestController.registerUser));
+/*
+회원가입 관련 필요 데이터
+uid, email, password, name, device, role, active
+*/
