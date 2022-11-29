27094b555d9590938cc360a2bd0b33b81c4586f1
seungha <frazzle124@gmail.com>
2022-11-29 12:52:06 +0900

    fix : 스케줄러 오전 3시 작동 수정

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.listen(port, () => {
   logger.info(`Server listening on port: ${port}`);
 });
 
-cron.schedule('0 59 23 * * *', async () => {
+cron.schedule('0 0 03 * * *', async () => {
   try {
     await api.createNewCase(1);
   } catch (e) {
